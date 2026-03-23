8bad6266a646eafd7bdb5875dfc57a975121b0ec
weizis <nastyamik2019@mail.ru>
2026-03-23 17:48:27 +0300

    Инициализация проекта

---
+++ users.js
@@ -0,0 +1,93 @@
+// users.js - данные всех пользователей
+const users = [
+    {
+        id: 1,
+        name: "Алина",
+        password: "24032007",
+        avatar: "assets/avatars/alina.jpg",
+        level: "КАЙФ",
+        bonus: "не платить аренду",
+        dramaProgress: 50,
+        dramaMax: 100
+    },
+    {
+        id: 2,
+        name: "Саша",
+        password: "26072007",
+        avatar: "assets/avatars/sasha.jpg",
+        level: "НОРМ",
+        bonus: "нет бонуса",
+        dramaProgress: 50,
+        dramaMax: 100
+    },
+    {
+        id: 3,
+        name: "Ваня",
+        password: "08082007",
+        avatar: "assets/avatars/vanya.jpg",
+        level: "НОРМ",
+        bonus: "нет бонуса",
+        dramaProgress: 50,
+        dramaMax: 100
+    },
+    {
+        id: 4,
+        name: "Тима",
+        password: "04022007",
+        avatar: "assets/avatars/tima.jpg",
+        level: "НОРМ",
+        bonus: "нет бонуса",
+        dramaProgress: 50,
+        dramaMax: 100
+    },
+    {
+        id: 5,
+        name: "Лада",
+        password: "27022007",
+        avatar: "assets/avatars/lada.jpg",
+        level: "НОРМ",
+        bonus: "нет бонуса",
+        dramaProgress: 50,
+        dramaMax: 100
+    },
+    {
+        id: 6,
+        name: "Катя",
+        password: "02102007",
+        avatar: "assets/avatars/katya.jpg",
+        level: "НОРМ",
+        bonus: "нет бонуса",
+        dramaProgress: 50,
+        dramaMax: 100
+    },
+    {
+        id: 7,
+        name: "Настя",
+        password: "28082007",
+        avatar: "assets/avatars/nastya.jpg",
+        level: "НОРМ",
+        bonus: "нет бонуса",
+        dramaProgress: 50,
+        dramaMax: 100
+    },
+    {
+        id: 8,
+        name: "Андрей",
+        password: "10012007",
+        avatar: "assets/avatars/andrey.jpg",
+        level: "НОРМ",
+        bonus: "нет бонуса",
+        dramaProgress: 50,
+        dramaMax: 100
+    },
+    {
+        id: 9,
+        name: "Василиса",
+        password: "08072007",
+        avatar: "assets/avatars/vasilisa.jpg",
+        level: "НОРМ",
+        bonus: "нет бонуса",
+        dramaProgress: 50,
+        dramaMax: 100
+    }
+];
